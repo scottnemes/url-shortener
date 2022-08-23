e1de52def26b2397fed97c254bf05556e6967676
Scott Nemes <scottmnemes@gmail.com>
2022-08-22 19:22:50 -0700

    Added env var lookup for custom log file path.

--- internal/config/config.go
+++ internal/config/config.go
@@ -42,20 +42,31 @@ func (c *Configuration) Init() {
 	c.CounterFile = fmt.Sprintf("%v/%v", c.ConfigDir, c.CounterFile)
 	c.TlsCrt = fmt.Sprintf("%v/%v", c.ConfigDir, c.TlsCrt)
 	c.TlsKey = fmt.Sprintf("%v/%v", c.ConfigDir, c.TlsKey)
+
 	// process flags
 	verbose := flag.Bool("v", false, "Enable debug output")
 	flag.Parse()
 	c.DebugMode = *verbose
+
+	// process env variables
+	if os.Getenv("USHORT_LOG_FILE") != "" {
+		c.LogFile = os.Getenv("USHORT_LOG_FILE")
+	}
 }
 
-const (
-	configFileName = "/etc/url_shortener/url_shortener.conf"
-)
-
 func LoadConfig() Configuration {
+	// check if a custom configuration file path is set
+	var configFileName string
+	if os.Getenv("USHORT_CONFIG_FILE") != "" {
+		configFileName = os.Getenv("USHORT_CONFIG_FILE")
+	} else {
+		configFileName = "/etc/url_shortener/url_shortener.conf"
+	}
+
+	// open and decode configuration file
 	configFile, err := os.Open(configFileName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Cannot load configuration file (%v)", err)
 	}
 	defer configFile.Close()
 
